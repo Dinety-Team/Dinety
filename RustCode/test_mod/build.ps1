ea1b018409ea97658550225865b02ba4b5e34f67
SCE21427 <SCE21427b@163.com>
2025-07-11 22:13:05 +0800

    Config Rust Lib

---
+++ RustCode/test_mod/build.ps1
@@ -0,0 +1,6 @@
+cargo check
+
+cargo build --target x86_64-pc-windows-msvc --release
+
+cargo build --target aarch64-linux-android --release
+
--- RustCode/test_mod/src/lib.rs
+++ RustCode/test_mod/src/lib.rs
@@ -1,4 +1,4 @@
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn add(left: u64, right: u64) -> u64 {
     left + right
 }
